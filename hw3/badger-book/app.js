756ffd1351f6dcb23e1787f0a70cc6b80630c6d4
Yunhao Jiang <account@yunhao.email>
2023-02-09 15:35:10 -0600

    Add more student data

--- hw3/badger-book/app.js
+++ hw3/badger-book/app.js
@@ -32,6 +32,10 @@ function buildStudentsHtml(studs) {
 function buildStudentHtml(stud) {
 	let html = `<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">`;
 	html += `<h2>${stud.name.first} ${stud.name.last}</h2>`;
-	html += `</div>`
+	html += `<strong>${stud.major}</strong>`;
+	html += `<p>${stud.name.first} is taking ${stud.numCredits} credits and ${stud.fromWisconsin ? "is" : "is not"} from Wisconsin.</p>`;
+	html += `<p> They have ${stud.interests.length} interests including...</p><ul>`;
+	html += stud.interests.map(interest => `<li>${interest}</li>`).join("\n");
+	html += `</ul></div>`
 	return html;
 }

f7978bc66392dd061efbaf229a91766672cf0b35
Yunhao Jiang <account@yunhao.email>
2023-02-09 15:27:01 -0600

    Fetch from API and add bootstrap column

--- hw3/badger-book/app.js
+++ hw3/badger-book/app.js
@@ -1,7 +1,15 @@
 /**
- * TODO Your code goes below here!
+ *
  * You may find the helper functions helpful.
  */
+fetch("https://cs571.org/s23/hw3/api/students",{
+	method: "GET",
+	headers: {
+		"X-CS571-ID": "bid_7d85b4cff564a5dc11dd"
+	}
+}).then(response => response.json()).then(data => {
+	document.getElementById("students").innerHTML = buildStudentsHtml(data);
+});
 
 /**
  * Given an array of students, generates HTML for all students
@@ -22,7 +30,7 @@ function buildStudentsHtml(studs) {
  * @returns 
  */
 function buildStudentHtml(stud) {
-	let html = `<div>`;
+	let html = `<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">`;
 	html += `<h2>${stud.name.first} ${stud.name.last}</h2>`;
 	html += `</div>`
 	return html;

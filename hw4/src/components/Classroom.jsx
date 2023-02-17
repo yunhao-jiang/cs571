fd389607176c1654ed7d03990917f80a8371741c
Yunhao Jiang <account@yunhao.email>
2023-02-16 18:01:06 -0600

    HW4 Initial Commit

---
+++ hw4/src/components/Classroom.jsx
@@ -0,0 +1,24 @@
+import { Button, Container, Form, Row } from "react-bootstrap";
+
+const Classroom = () => {
+    return <div>
+        <Form>
+            <Form.Label htmlFor="searchName">Name</Form.Label>
+            <Form.Control id="searchName"/>
+            <Form.Label htmlFor="searchMajor">Major</Form.Label>
+            <Form.Control id="searchMajor"/>
+            <Form.Label htmlFor="searchInterest">Interest</Form.Label>
+            <Form.Control id="searchInterest"/>
+            <br />
+            <Button variant="neutral">Reset Search</Button>
+        </Form>
+        <Container fluid>
+            <Row>
+                { /* TODO Students go here! */ }
+            </Row>
+        </Container>
+    </div>
+
+}
+
+export default Classroom;

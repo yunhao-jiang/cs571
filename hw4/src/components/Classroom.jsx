bffbf36434890e2752a63b7e99a86908219b3d31
Yunhao Jiang <account@yunhao.email>
2023-02-19 23:37:10 -0600

    Complete step 1, 2, and 3

--- hw4/src/components/Classroom.jsx
+++ hw4/src/components/Classroom.jsx
@@ -1,6 +1,32 @@
-import { Button, Container, Form, Row } from "react-bootstrap";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
+import Student from "./Student";
+import {useEffect, useState} from "react";
 
 const Classroom = () => {
+    function addStudent() {
+        fetch("https://cs571.org/s23/hw4/api/students",
+            {
+                method: "GET",
+                headers: {
+                    "X-CS571-ID": "bid_7d85b4cff564a5dc11dd"
+                }
+            }).then(response => response.json()).then(data => {
+            data.map((student) => {
+                    const name = student.name;
+                    const studentId = student.id;
+                    // const major = student.major;
+                    // const interests = student.interests;
+                    setStudents(oldStudents => [...oldStudents, {name, studentId}]);
+                }
+            )
+        });
+    }
+
+
+    const [students, setStudents] = useState([]);
+    useEffect(() => {
+        addStudent();
+    }, [])
     return <div>
         <Form>
             <Form.Label htmlFor="searchName">Name</Form.Label>
@@ -9,12 +35,17 @@ const Classroom = () => {
             <Form.Control id="searchMajor"/>
             <Form.Label htmlFor="searchInterest">Interest</Form.Label>
             <Form.Control id="searchInterest"/>
-            <br />
+            <br/>
             <Button variant="neutral">Reset Search</Button>
         </Form>
         <Container fluid>
-            <Row>
-                { /* TODO Students go here! */ }
+            <Row>{
+                students.map((student) => {
+                    return <Col xs={12} sm={6} md={4} lg={3} xl={2} key={student.studentId}>
+                        <Student name={student.name}/>
+                    </Col>
+                })
+            }
             </Row>
         </Container>
     </div>

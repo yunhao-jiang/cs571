68f493ff7a8aea6e137fdc1ca6082394859950e4
Yunhao Jiang <account@yunhao.email>
2023-03-11 22:48:30 +0800

    Complete register module

--- hw6/src/components/auth/BadgerRegister.js
+++ hw6/src/components/auth/BadgerRegister.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import {Button, Form} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 export default function BadgerRegister() {
 
-    // TODO Create the register component.
+    const [userName, setUserName] = React.useState('');
+    const [password1, setPassword1] = React.useState('');
+    const [password2, setPassword2] = React.useState('');
+    const navigate = useNavigate();
 
-    return <>
-        <h1>Register</h1>
-    </>
-}
+    const register = useCallback(() => {
+        if (userName.length === 0 || password1.length === 0) {
+            alert('You must provide both a username and password!');
+            return;
+        }
+        if (password1 !== password2) {
+            alert('Your passwords do not match!');
+            return;
+        }
+        fetch('https://www.cs571.org/s23/hw6/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CS571-ID': 'bid_7d85b4cff564a5dc11dd'
+            },
+            body: JSON.stringify({
+                username: userName,
+                password: password1
+            })
+        }).then(res => {
+            if (res.status === 200) {
+                alert('You have successfully registered!');
+                navigate('/')
+                return
+            }
+            return res.json()
+        }).then(json => {
+            if (json && json.msg) alert(json.msg);
+        })
+    });
+
+        return <>
+            <h1>Register</h1>
+            <Form>
+                <Form.Label htmlFor="userName">Username</Form.Label>
+                <Form.Control type="text" value={userName}
+                              onChange={e => setUserName(e.target.value)}/>
+                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Control type="password" value={password1}
+                              onChange={e => setPassword1(e.target.value)}/>
+                <Form.Label htmlFor="passwordRepeat">Repeat Password</Form.Label>
+                <Form.Control type="password" value={password2}
+                              onChange={e => setPassword2(e.target.value)}/>
+            </Form>
+            <Button onClick={register}>Register</Button>
+        </>
+    }

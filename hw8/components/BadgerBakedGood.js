763d6fd8c6468e0b5fcbaeaf6a05a0e4f1c51cfe
Yunhao Jiang <account@yunhao.email>
2023-04-03 22:47:24 -0500

    Complete step 1&2

--- hw8/components/BadgerBakedGood.js
+++ hw8/components/BadgerBakedGood.js
@@ -1,8 +1,22 @@
-import { Text, View } from "react-native";
+import {Text, View, Image, StyleSheet} from "react-native";
+import React from "react";
 
 export default function BadgerBakedGood(props) {
-    
-    return <View>
-        <Text>I am a baked good!</Text>
+
+    return <View style={styles.container}>
+        <Image source={{uri: props.img}} style={{width: 150, height: 150}}/>
+        <Text style={{fontSize: 30}}>{props.name}</Text>
+        <Text> </Text>
+        <Text style={styles.text}>${props.price}</Text>
+        <Text style={styles.text}>You can order up to {props.upperBound} units!</Text>
     </View>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 17,
+    }
+})
--- hw8/components/BadgerBakery.js
+++ hw8/components/BadgerBakery.js
@@ -1,12 +1,45 @@
-import { Text, View } from "react-native";
+import {Button, Text, View} from "react-native";
+import {useEffect, useState} from "react";
 import BadgerBakedGood from "./BadgerBakedGood";
 
 export default function BadgerBakery() {
+    const [bakeryItems, setBakeryItems] = useState({})
+    const [bakeryKeys, setBakeryKeys] = useState([])
+    const [prevDisabled, setPrevDisabled] = useState(true)
+    const [nextDisabled, setNextDisabled] = useState(false)
+    const [currentItem, setCurrentItem] = useState(0)
+
+    useEffect(() => {
+        fetch("https://www.cs571.org/s23/hw8/api/bakery/items", {
+            method: "GET",
+            headers: {
+                "X-CS571-ID": "bid_7d85b4cff564a5dc11dd"
+            }
+        }).then(res => res.json()).then(json => {
+            setBakeryItems(json)
+            setBakeryKeys(Object.keys(json))
+            console.log(bakeryKeys)
+        })
+    }, [])
+
+    const prevItem = (() => {
+        if(currentItem === 1) setPrevDisabled(true)
+        if(currentItem === bakeryKeys.length - 1) setNextDisabled(false)
+        setCurrentItem(currentItem - 1)
+    })
+
+    const nextItem = (() => {
+        if(currentItem === bakeryKeys.length - 2) setNextDisabled(true)
+        if(currentItem === 0) setPrevDisabled(false)
+        setCurrentItem(currentItem + 1)
+    })
+
 
     return <View>
-        <Text>Welcome to Badger Bakery!</Text>
-        <BadgerBakedGood/>
-        <BadgerBakedGood/>
-        <BadgerBakedGood/>
+        <Text style={{alignSelf: "center"}}>Welcome to Badger Bakery!</Text>
+        <Button disabled={prevDisabled} onPress={prevItem} title="PREV"/>
+        <Button disabled={nextDisabled} onPress={nextItem} title="NEXT"/>
+        <BadgerBakedGood key={bakeryKeys[currentItem]}
+                         name={bakeryKeys[currentItem]} {...bakeryItems[bakeryKeys[currentItem]]}/>
     </View>
 }

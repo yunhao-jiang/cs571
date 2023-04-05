8e5e2f74ccd5f1e3f9b1c1e6cd66e8d0505765ec
Yunhao Jiang <account@yunhao.email>
2023-04-05 15:58:37 -0500

    Modified cart representation as required

--- hw8/components/BadgerBakedGood.js
+++ hw8/components/BadgerBakedGood.js
@@ -1,14 +1,16 @@
 import {Text, View, Image, StyleSheet} from "react-native";
-import React, {useState} from "react";
+import React from "react";
 
 
 export default function BadgerBakedGood(props) {
+
 
     return <View style={styles.container}>
         <Image source={{uri: props.img}} style={{width: 150, height: 150}}/>
         <Text style={{fontSize: 30}}>{props.name}</Text>
         <Text> </Text>
-        <Text style={styles.text}>${props.price}</Text>
+        <Text
+            style={styles.text}>${(props.price) === undefined ? 0 : (props.price).toFixed(2)}</Text>
         <Text style={styles.text}>You can order up to {props.upperBound} units!</Text>
     </View>
 }
--- hw8/components/BadgerBakery.js
+++ hw8/components/BadgerBakery.js
@@ -1,18 +1,18 @@
-import {Button, Text, View} from "react-native";
-import React, {useContext, useEffect, useState} from "react";
+import {Button, Text, View, StyleSheet} from "react-native";
+import React, {useEffect, useState} from "react";
 import BadgerBakedGood from "./BadgerBakedGood";
-import BakeryOrderContext from "../contexts/BakeryOrderContext";
 
 export default function BadgerBakery() {
     const [bakeryItems, setBakeryItems] = useState({})
     const [bakeryKeys, setBakeryKeys] = useState([])
     const [prevDisabled, setPrevDisabled] = useState(true)
     const [nextDisabled, setNextDisabled] = useState(false)
-    const [currentItem, setCurrentItem] = useState(0)
+    const [currIndex, setCurrIndex] = useState(0)
     const [cart, setCart] = useState({})
     const [quantity, setQuantity] = useState(0)
     const [minusDisabled, setMinusDisabled] = useState(true)
     const [plusDisabled, setPlusDisabled] = useState(false)
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(() => {
         fetch("https://www.cs571.org/s23/hw8/api/bakery/items", {
@@ -22,56 +22,9 @@ export default function BadgerBakery() {
         }).then(res => res.json()).then(json => {
             setBakeryItems(json)
             setBakeryKeys(Object.keys(json))
+            setLoaded(true)
         })
     }, [])
-
-    const prevItem = (() => {
-        if (currentItem === 1) setPrevDisabled(true)
-        if (currentItem === bakeryKeys.length - 1) setNextDisabled(false)
-        setCurrentItem(currentItem - 1)
-        setQuantity(0)
-        setMinusDisabled(true)
-        setPlusDisabled(false)
-    })
-
-    const nextItem = (() => {
-        if (currentItem === bakeryKeys.length - 2) setNextDisabled(true)
-        if (currentItem === 0) setPrevDisabled(false)
-        setCurrentItem(currentItem + 1)
-        setQuantity(0)
-        setMinusDisabled(true)
-        setPlusDisabled(false)
-    })
-
-    const minusItem = (() => {
-        if (quantity === 1) setMinusDisabled(true)
-        if (quantity === bakeryItems[bakeryKeys[currentItem]].upperBound) setPlusDisabled(false)
-        setQuantity(quantity - 1)
-    })
-
-    const plusItem = (() => {
-        if (quantity === bakeryItems[bakeryKeys[currentItem]].upperBound - 1) setPlusDisabled(true)
-        if (quantity === 0) setMinusDisabled(false)
-        setQuantity(quantity + 1)
-    })
-
-    const addToCart = (() => {
-        const existingQuantity = cart[bakeryKeys[currentItem]]
-        let afterQuantity = 0
-
-        existingQuantity ? afterQuantity = existingQuantity + quantity : afterQuantity = quantity
-
-        if (afterQuantity > bakeryItems[bakeryKeys[currentItem]].upperBound) {
-            alert("You can only order up to " + bakeryItems[bakeryKeys[currentItem]].upperBound + " units of " + bakeryKeys[currentItem])
-        } else {
-            setCart({...cart, [bakeryKeys[currentItem]]: afterQuantity})
-
-        }
-        setQuantity(0)
-        setMinusDisabled(true)
-        setPlusDisabled(false)
-    })
-
 
     const calcTotal = (() => {
         let total = 0
@@ -91,22 +44,61 @@ export default function BadgerBakery() {
         })
         setCart({})
         setQuantity(0)
-        setMinusDisabled(true)
-        setPlusDisabled(false)
     })
+
+    useEffect(() => {
+        // Check PREV and NEXT buttons
+        if (currIndex === 0) setPrevDisabled(true)
+        else setPrevDisabled(false)
+        if (currIndex === bakeryKeys.length - 1) setNextDisabled(true)
+        else setNextDisabled(false)
+        // Set quantity from cart, 0 if not in cart
+        cart[bakeryKeys[currIndex]] !== undefined ? setQuantity(cart[bakeryKeys[currIndex]]) : setQuantity(0)
+    }, [currIndex])
+
+    useEffect(() => {
+        if (loaded) {
+            // Add to cart once quantity changed
+            if (quantity >= 0) {
+                setCart({...cart, [bakeryKeys[currIndex]]: quantity})
+            }
+        }
+    }, [quantity])
+
+    useEffect(() => {
+        if (loaded) {
+            if (quantity === 0) setMinusDisabled(true)
+            else setMinusDisabled(false)
+            if (quantity === bakeryItems[bakeryKeys[currIndex]].upperBound) setPlusDisabled(true)
+            else setPlusDisabled(false)
+        }
+    }, [quantity, currIndex])
 
 
     return <View>
         <Text style={{alignSelf: "center"}}>Welcome to Badger Bakery!</Text>
-        <Button disabled={prevDisabled} onPress={prevItem} title="PREV"/>
-        <Button disabled={nextDisabled} onPress={nextItem} title="NEXT"/>
-        <BadgerBakedGood key={bakeryKeys[currentItem]}
-                         name={bakeryKeys[currentItem]} {...bakeryItems[bakeryKeys[currentItem]]}/>
-        <Button disabled={minusDisabled} onPress={minusItem} title="-"/>
-        <Text>{quantity}</Text>
-        <Button disabled={plusDisabled} onPress={plusItem} title="+"/>
-        <Button onPress={addToCart} title="ADD TO CART"/>
-        <Text>Order Total: ${calcTotal().toFixed(2)}</Text>
+        <View style={styles.button}>
+            <Button disabled={prevDisabled} onPress={() => setCurrIndex(currIndex - 1)}
+                    title="PREV"/>
+            <Button disabled={nextDisabled} onPress={() => setCurrIndex(currIndex + 1)}
+                    title="NEXT"/>
+        </View>
+        <BadgerBakedGood key={bakeryKeys[currIndex]}
+                         name={bakeryKeys[currIndex]} {...bakeryItems[bakeryKeys[currIndex]]}/>
+        <View style={styles.button}>
+            <Button disabled={minusDisabled} onPress={() => setQuantity(quantity - 1)} title="-"/>
+            <Text>{quantity}</Text>
+            <Button disabled={plusDisabled} onPress={() => setQuantity(quantity + 1)} title="+"/>
+        </View>
+        <Text style={{alignSelf: 'center'}}>Order Total: ${calcTotal().toFixed(2)}</Text>
         <Button onPress={submitOrder} title="SUBMIT ORDER" disabled={calcTotal() === 0}/>
     </View>
 }
+
+const styles = StyleSheet.create({
+    button: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
